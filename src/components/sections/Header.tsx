40e291413c0ac182c7b48ac046945f5582445804
Na11a <60902599+Na11a@users.noreply.github.com>
2022-01-24 12:39:10 +0200

    add width for footer adnd headre

--- src/components/sections/Header.tsx
+++ src/components/sections/Header.tsx
@@ -2,9 +2,17 @@ import React from "react";
 import styled from "styled-components";
 import { SocialMedia } from "..";
 import { SearchSection } from ".";
+import { media } from "../../styles";
 
 type Props = {};
-const StyledHeader = styled.header``;
+const StyledHeader = styled.header`
+  width:90%;
+  padding: 0 5%;
+  ${media.greaterThan("sm")`
+  width:80%;
+  padding: 0 10%;
+  `}
+`;
 const Header = (props: Props) => {
   return (
     <StyledHeader>
