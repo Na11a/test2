b798c400ec8eaa772a266739c417f376bd0b24d1
Na11a <60902599+Na11a@users.noreply.github.com>
2022-01-25 17:43:51 +0200

    indexes

---
+++ src/components/HomePage/index.ts
@@ -0,0 +1,4 @@
+export {default as ContactInvitation} from './ContactInvitation'
+export {default as HowCreatePost} from './HowCreatePost'
+export {default as ScreenCards} from './ScreenCards'
+export {default as ScreenCard} from './ScreenCard'
